a1c1c9ea5a636cf7bcafe2f2da0c50b6a15cb9f0
Aleksey Kuzmin <kboomska@gmail.com>
2023-08-04 13:52:20 +0300

    Model comparison (override '==' and 'hashCode')

--- freezed_example/lib/models/user_model.dart
+++ freezed_example/lib/models/user_model.dart
@@ -6,4 +6,14 @@ class User {
     required this.name,
     required this.age,
   });
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is User && other.name == name && other.age == age;
+  }
+
+  @override
+  int get hashCode => name.hashCode ^ age.hashCode;
 }
